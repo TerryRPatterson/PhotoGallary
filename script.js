73b481065cec7d6a87024dd8971ac92d795df4d6
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-02-28 09:09:01 -0500

    Finished jQuery Conversion  closes #4

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
-/*global EXIF*/
+/*global EXIF $*/
+/*eslint no-console: 0*/
 let lightBox = function lightBox(event){
-    let lightbox = document.querySelector(".lightbox");
-    let lightBoxImg = document.querySelector(".lightbox  img");
+    let $lightbox = $(".lightbox");
+    let $lightBoxImg = $(".lightbox  img");
     if (event.target.classList.contains("lightbox")){
-        lightbox.classList.toggle("active");
-        document.querySelector("img.active").classList.toggle("active");
+        $lightbox.toggleClass("active");
+        $("img.active").toggleClass("active");
     }
     else if(event.target.classList.contains("galleryItem")){
-        let metadataFrame = document.querySelector(".metadataFrame");
+        let $metadataFrame = $(".metadataFrame");
 
-        lightBoxImg.setAttribute("src", event.target.getAttribute("src"));
-        lightbox.classList.toggle("active");
+        $lightBoxImg.attr("src", event.target.getAttribute("src"));
+        $lightbox.toggleClass("active");
 
         event.target.classList.toggle("active");
         // Getting MetaData
-        metadataFrame.textContent = "";
-        let PictureMetadata = Pictures[event.target.getAttribute("id")];
-        for (let tag in PictureMetadata){
-            metadataFrame.textContent += tag + " " + PictureMetadata[tag];
-            metadataFrame.textContent +="\n";
-        }
+        $metadataFrame.text(function(){
+            let textContent = "";
+            let PictureMetadata = Pictures[event.target.getAttribute("data-id")];
+            for (let tag in PictureMetadata){
+                textContent += tag + " " + PictureMetadata[tag];
+                textContent +="\n";
+            }
+            return textContent;
+        });
     }
 
 };
 
-let getMetaData = function getMetaData(image,index){
-    let metadata = {};
-    EXIF.getData(image, function () {
-        let tags = EXIF.getAllTags(this);
-        for (let tag in tags){
-            if (tag !== "thumbnail"){
-                metadata[tag] = tags[tag];
-                console.log(urls[index]);
-                console.log(metadata[tag],tag);
-            }
-        }
-        Pictures[index] = metadata;
-        metadataLoaded ++;
-        console.log(metadataLoaded);
-        if (metadataLoaded === urls.length){
-            galleryHolder.forEach(function(tag){
-
-                Gallery.appendChild(tag);
+let getMetaData = function getMetaData(){
+    console.log(imagesLoaded + " imagesLoaded");
+    if (imagesLoaded === (urls.length - 1)){
+        console.log("Get MetaData called.");
+        let metadata = {};
+        let $pictures = $(".galleryItem");
+        $pictures.each(function(index){
+            EXIF.getData(this, function () {
+                let tags = EXIF.getAllTags(this);
+                for (let tag in tags){
+                    if (tag !== "thumbnail"){
+                        metadata[tag] = tags[tag];
+                        console.log(urls[index]);
+                        console.log(metadata[tag],tag);
+                    }
+                }
+                Pictures[index] = metadata;
+                console.log(metadata);
             });
-        }
-        console.log(metadata);
-    });
+        });
+    }
 };
 let urls = ["Pictures/IMG_01.jpg","Pictures/IMG_02.jpg",
     "Pictures/IMG_03.jpg","Pictures/IMG_04.jpg","Pictures/IMG_05.jpg",
     "Pictures/ANIM_01.gif"];
 
 let Gallery = document.querySelector(".gallery");
-let galleryHolder = [];
+
 
 
 document.querySelector("body").addEventListener("click",lightBox);
 let Pictures = [];
-let metadataLoaded = 0;
+let imagesLoaded = 0;
 urls.forEach(function(url, index) {
     let galleryItem = document.createElement("img");
     galleryItem.classList.add("galleryItem");
     galleryItem.setAttribute("src",url);
-    galleryItem.setAttribute("id", index);
+    galleryItem.setAttribute("data-id", index);
     //retrive metadata
     getMetaData(galleryItem,index);
-
-    galleryHolder.push(galleryItem);
+    Gallery.appendChild(galleryItem);
+    imagesLoaded ++;
 });
 console.log(Pictures.length);
-console.log("Load complete.");
+$(".gallery").on("load",getMetaData);
